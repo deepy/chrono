7e88c0663c755ffe3e505843b760f69512f8b9a4
Alex Nordlund <deep.alexander@gmail.com>
2022-07-23 10:44:52 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from distutils.core import setup
 setup(name='chrono',
       version='0.0.1',
+      py_modules=["chrono", "today"],
+      install_requires=["humanize==0.5.1"],
       entry_points={
           'console_scripts': [
               'chrono = chrono:launch',
